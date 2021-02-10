20ff3827e5f6d1cca66bd8a4a6e57a305e8c0e89
bouch mana <mana99hotmail.fr>
2021-02-10 13:14:24 +0100

    rajout de ligne

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -69,8 +69,8 @@ public class EmployeService {
 
         //Création et sauvegarde en BDD de l'employé.
         Employe employe = new Employe(nom, prenom, matricule, LocalDate.now(), salaire, Entreprise.PERFORMANCE_BASE, tempsPartiel);
-//        employe = employeRepository.save(employe);
-       // logger.info("Employé créé : {}", employe.toString());
+       // employe = employeRepository.save(employe);
+        //logger.info("Employé créé : {}", employe.toString());
         employeRepository.save(employe);
        // return employe;
 
@@ -136,7 +136,7 @@ public class EmployeService {
         if(performanceMoyenne != null && performance > performanceMoyenne){
             performance++;
         }
-
+        //lolilol
         //Affectation et sauvegarde
         employe.setPerformance(performance);
         employeRepository.save(employe);
